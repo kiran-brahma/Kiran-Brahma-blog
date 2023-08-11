8ccd4a22a89b8e7ddcdb893ff90730541208e79c
Kiran Brahma <107672627+kiran-brahma@users.noreply.github.com>
2023-08-11 23:41:07 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,24 @@ const { withContentlayer } = require("next-contentlayer");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/pages/:path*',
+        destination: '/:path*',
+        permanent: true,
+      },
+    ]
+  },
+  images: {
+    domains: [
+      'i.imgur.com',
+      'cdn-icons-png.flaticon.com',
+      "cdn.hashnode.com",
+    ],
+  },
+  
+
   swcMinify: true,
 };
 
